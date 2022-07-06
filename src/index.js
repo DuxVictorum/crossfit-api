2c64f32e6b9a40c0d57957f5cb45c8420e8ff0b5
Mark Thorne <thorne.mark@gmail.com>
2022-07-06 00:00:34 -0500

    Work on index.js to handle JSON

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
+const bodyParser = require("body-parser");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ app.get("/", (req, res) => {
   res.send("<h2>It seems to be working!</h2>");
 });
 
+app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
