c2970124812ee62ffe53c764ef846513b4bbe944
Mark Thorne <thorne.mark@gmail.com>
2022-07-06 23:32:46 -0500

    modified controller to handle input JSON for making a new workout

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -2,12 +2,29 @@ const workoutService = require("../services/workoutService");
 
 const getAllWorkouts = (req, res) => {
   const allWorkouts = workoutService.getAllWorkouts();
-  res.send({ status:"OK", data: allWorkouts });
+  res.send({ status: "OK", data: allWorkouts });
 };
 
 const getOneWorkout = (req, res) => {
-  const workout = workoutService.getOneWorkout();
-  res.send("Get an existing workout");
+  const { body } = req;
+  if (
+    !body.name ||
+    !body.mode ||
+    !body.equipment ||
+    !body.exercises ||
+    !body.trainerTips
+  ) {
+    return;
+  }
+  const newWorkout = {
+    name: body.name,
+    mode: body.mode,
+    equipment: body.equipment,
+    exercises: body.exercises,
+    trainerTips: body.trainerTips,
+  };
+  const createdWorkout = workoutService.createNewWorkout(newWorkout);
+  res.status(201).send({ status: "OK", data: createdWorkout });
 };
 
 const createNewWorkout = (req, res) => {
