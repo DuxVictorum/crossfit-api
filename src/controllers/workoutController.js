99313a59a8b00ddb0054afdcd736db141cb6b2ab
Mark Thorne <thorne.mark@gmail.com>
2022-07-13 23:50:16 -0500

    Begun adding error messages to controller

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -25,6 +25,14 @@ const createNewWorkout = (req, res) => {
     !body.exercises ||
     !body.trainerTips
   ) {
+    res
+      .status(400)
+      .send({
+        status: "FAILED",
+        data: {
+          error: "One of the following keys is missing or empty in request body: 'name', 'mode', 'equipment', 'exercises', 'trainerTips'",
+        },
+      });
     return;
   }
   const newWorkout = {
