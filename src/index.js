af0a43ec1b970c2d475b32b0efe0df33a7e1357e
Mark Thorne <thorne.mark@gmail.com>
2022-08-01 22:55:07 -0500

    Added cache capability

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000;
 
 // For initial testing purposes only
@@ -11,6 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(bodyParser.json());
+app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
