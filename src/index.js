c69d9fbf35225d6e2e948505cf6b8739b0f996a3
Mark Thorne <thorne.mark@gmail.com>
2022-08-04 14:20:16 -0500

    Finished basic setup of Swagger docs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// Code from the project website:
+// https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const { swaggerDocs: V1SWaggerDocs } = require("./v1/swagger");
 
 const app = express();
 const cache = apicache.middleware;
@@ -18,4 +22,5 @@ app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}.`);
+  V1SWaggerDocs(app, PORT);
 });
